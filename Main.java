52d89d356993ad7b04b67c8e12470033175ebe6b
Ahmed <medizo335@gmail.com>
2026-08-19 22:23:28 +0300

    Create my portfolio website

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Linux!");
+        System.out.println("I am learning Java with VS Code!");
+
+    }
+}
